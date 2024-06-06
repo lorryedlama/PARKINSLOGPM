13e38f32df864d1efdf6f0d38e60965a1c5e7c10
lorryedlama <lorryed75@gmail.com>
2024-06-06 09:17:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import asyncio
 from flask import Flask
 from keep_alive import keep_alive
 
+responses = [
+    "Thx",
+    "Thanks",
+    "Ty",
+    "thx",
+    "ty",
+    "thanks",
+    "thank you",
+    "Thank you",
+    "lfg",
+    "Thank you",
+]
+
 keep_alive()
 
 app = Flask(__name__)
@@ -18,16 +31,22 @@ port = os.environ.get('PORT', 5000)  # Default to 5000 if PORT environment varia
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
+client = MyClient()
+@client.event
+async def on_message(message):
+    if message.author.id == 1150448986264698980:
+        # Check if the bot is mentioned and the message contains the embed title
+        if client.user.mentioned_in(message) and any("### 🎟️\xa0\xa0Raffle ended!" in embed.description for embed in message.embeds):
+            response = random.choice(responses)
+            await asyncio.sleep(random.randint(5, 20))
+            await message.channel.send(response)
 
-    async def on_message(self, message):
-        if (message.author.id == 1150448986264698980):
-            print(f'Message received user {message.author.name}: {message.content}')
-            for component in message.components:
-                for child in component.children:
-                    if child.label == "Enter":
-                        await asyncio.sleep(random.randint(5, 10))
-                        await child.click()
+        # Your existing code for the raffle functionality with components
+        for component in message.components:
+            for child in component.children:
+                if child.label == "Enter":
+                    await asyncio.sleep(random.randint(5, 10))
+                    await child.click()
 
 if __name__ == "__main__":
-    client = MyClient()
     client.run(os.environ['TOKEN'])
