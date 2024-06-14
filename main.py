f17279e6ec69a4f0124c4bc6b69265b4ca0bdbdb
lorryedlama <lorryed75@gmail.com>
2024-06-14 19:52:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,33 +34,11 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
-    async def join_voice_channel(self, channel_id):
-        voice_channel = self.get_channel(channel_id)
-        if voice_channel:
-            if not self.voice_clients:
-                await voice_channel.connect()
-            else:
-                await self.voice_clients[0].move_to(voice_channel)
-
-    async def leave_voice_channel(self):
-        if self.voice_clients:
-            await self.voice_clients[0].disconnect()
-
 client = MyClient()
 
 @client.event
 async def on_message(message):
-    if message.content == '!aji' and message.author.id == 740547277164249089:
-        print("Command received from correct user.")
-        await asyncio.sleep(random.randint(5, 20))
-        await client.join_voice_channel(1239293213525803052)
-
-    if message.content == '!st9':  # New condition for leaving voice channel
-        print("Leaving voice channel.")
-        await asyncio.sleep(random.randint(1, 6))
-        await client.leave_voice_channel()
-
-    if message.author.id == 1150448986264698980 and message.guild.id == 1222623160734580736:
+    if message.author.id == 1150448986264698980 and (message.guild.id == 1222623160734580736 or message.guild.id == 930591458682081301):
         print("Message from bot.")
         for embed in message.embeds:
             if client.user.mentioned_in(message) and "### 🎟️\xa0\xa0Raffle ended!" in embed.description:
